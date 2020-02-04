894c4107bc21467b8812f8581c1d9f1cb3782623
rohitkuber2000 <46227008+rohitkuber2000@users.noreply.github.com>
2020-02-04 12:50:17 +0530

    Delete helloWorld.java

--- helloWorld.java
+++
@@ -1,9 +0,0 @@
-
-public class helloWorld
-{
-    public static void main()
-    {
-        System.out.println("HELLO WORLD:)");
-        System.out.println("Welcome to ADG and java syntax is too long :) ");
-    }
-}
