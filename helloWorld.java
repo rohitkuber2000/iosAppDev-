d39651e265d01073d095808bd3400ebfbf44f462
rohitkuber2000 <46227008+rohitkuber2000@users.noreply.github.com>
2020-02-04 12:45:55 +0530

    Update helloWorld.java
    Added a new print statement

--- helloWorld.java
+++ helloWorld.java
@@ -4,5 +4,6 @@ public class helloWorld
     public static void main()
     {
         System.out.println("HELLO WORLD:)");
+        System.out.println("Welcome to ADG and java syntax is too long :) ");
     }
 }
